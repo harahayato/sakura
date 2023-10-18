a6ed2a81ccb66da774c9af0425cdf498d1cc92ee
sushi-eat <hashida.haruna.21@shizuoka.ac.jp>
2023-10-18 15:39:31 +0900

    Change code^^

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 # ライブラリのインポート
 import requests
 import math
+import json
 
 f = open('myfile.txt', 'r', encoding='UTF-8')
 
@@ -9,37 +10,46 @@ print(text)
 
 f.close()
 
-def point(self):
     
 # APIに接続するための情報
-    API_Endpoint = 'http://ap.mextractr.net/ma9/negaposi_analyzer'
-    API_Key = "<API_KEY>"
+API_Endpoint = 'http://ap.mextractr.net/ma9/negaposi_analyzer'
+#API_Key = 'api_key'
 
 # リクエストヘッダを指定
-    headers = {
-        "content-type":"application/json"
-        }
+headers = {
+    "content-type":"application/json"
+    }
 
 # URLパラメータを指定
-    params = {
-        'apikey':'',
-        'out':'json',
-        'text':text
-        }
+params = {
+    'apikey':'',
+    'out':'json',
+    'text':text
+    }
 
 # request.getを使いレスポンスオブジェクトとしてresultをたてる
-    result=requests.get(API_Endpoint,headers=headers,params=params)
-        # json()メソッドを使うとレスポンスの内容を辞書または辞書のリストに変換して取得
-    json_data = result.json()
-    # negaposi要素(=ネガポジ度合いの数値)をaにいれる
-    a = json_data['negaposi']
+result=requests.get(API_Endpoint,headers=headers,params=params)
+print(result)
 
-    # aが0以上だった場合には切り捨て+1、0未満だった場合には全てパラメータを0に変換
-    if(a>=0):
-        a = math.floor(a)+1
-    else:
-        a=0
+# json()メソッドを使うとレスポンスの内容を辞書または辞書のリストに変換して取得
+json_data = result.json()
+print(json_data)
 
-    print(a)
+#json形式のデータをファイルで保存
+filename = "result.json"
+with open (filename, "w") as fp:
+    json.dump(json_data, fp)
 
-    return a
+# negaposi要素(=ネガポジ度合いの数値)をaにいれる
+a = json_data['negaposi']
+
+print(a)
+
+# aが0以上だった場合には切り捨て+1、0未満だった場合には全てパラメータを0に変換
+if(a>=0):
+    a = math.floor(a)+1
+else:
+    a=0
+    
+print(a)
+
